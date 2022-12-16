7d2525db679f7fbc568cdc039e16b5b2a2f34efb
Rodrigo Flores <rfloresa1900@alumno.ipn.mx>
2022-12-15 23:33:38 -0600

    Clientes, listar y eliminar

--- src/Components/Clientes/Clientes.js
+++ src/Components/Clientes/Clientes.js
@@ -1,9 +1,35 @@
 import React from 'react'
-import { Card, CardContent, ButtonGroup, Button } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { Card, CardContent, ButtonGroup, Button, Typography } from '@mui/material';
 import {useNavigate} from 'react-router-dom'
 
 function Clientes(){
+    const [clients, setClients]= useState([]);
     const navigate=useNavigate();
+
+    const loadClient=async()=>{
+        try {
+            const response= await fetch('http://localhost:4000/readcliente');
+            const data=await response.json();
+            setClients(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    const handleDelete=async(telefono)=>{
+        try {
+            await fetch(`http://localhost:4000/deletecliente/${telefono}`,{
+                method: 'DELETE'
+            });
+            setClients(clients.filter((client)=>client.telefono !== telefono))
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(()=>{
+        loadClient();
+    },[])
     return(
         <>
             <div style={{margin: '1rem', alignItems: 'center', textAlign: 'center'}}>
@@ -17,6 +43,66 @@ function Clientes(){
                     </ButtonGroup>
                 </Card>
             </div>
+            {
+                clients.map(client=>(
+                    <Card style={{
+                        marginBottom: '.9rem',
+                        backgroundColor: '#689FF2',
+                        color: '#white'
+                    }}>
+                        <CardContent style={{
+                            display: "flex",
+                            justifyContent: "space-between"
+                        }}>
+                            <div>
+                                <Typography style={{color:'white'}}>
+                                    <h3>
+                                        {client.nombre}
+                                    </h3>
+                                </Typography>
+                                <Typography>
+                                    <h4>
+                                        {client.apellidoP}
+                                    </h4>
+                                    <h4>
+                                        {client.apellidoM}
+                                    </h4>
+                                    <h4>
+                                        {client.refdir}
+                                    </h4>
+                                    <h4>
+                                        {client.telefono}
+                                    </h4>
+                                    <h4>
+                                        {client.lada}
+                                    </h4>
+                                    <h4>
+                                        {client.email}
+                                    </h4>
+                                    <h4>
+                                        {client.direccion}
+                                    </h4>
+                                </Typography>
+                            </div>
+                            <div>
+                                <Button variant='contained' 
+                                color='inherit' 
+                                onClick={()=>navigate(`/clientes/${client.id}/editar`)}>
+                                    Editar
+                                </Button>
+                                <Button 
+                                variant='contained' 
+                                color='secondary' 
+                                onClick={()=>handleDelete(client.telefono)}
+                                style={{margin:'.6rem'}}
+                                >
+                                    Eliminar
+                                </Button>
+                            </div>
+                        </CardContent>
+                    </Card>
+                ))
+            }
             <Card style={{
                 marginBottom: '.9rem',
                 backgroundColor: '#689FF2',
--- src/Components/Clientes/CrearClientes.js
+++ src/Components/Clientes/CrearClientes.js
@@ -1,8 +1,108 @@
+import { Button, Card, CardContent, Grid, TextField, Typography } from '@mui/material';
 import React from 'react'
 
 function CrearClientes(){
     return(
-        <h2>CrearClientes</h2>
+        <>
+            <h2>CrearClientes</h2>
+            <Grid 
+                container
+                direction='column'
+                alignItems='center'
+                justifyContent='center'
+            >
+                <Grid item xs={3}>
+                    <Card
+                        sx={{mt:5}}
+                        style={{
+                            padding: '1rem',
+                            backgroundColor: '#1976d2'
+                        }}
+                    >
+                        <Typography variant='5' textAlaign='center' color='white'>
+                            Crear/editar Clientes
+                        </Typography>
+                        <CardContent>
+                            <form>
+                                <TextField
+                                    variant='outlined'
+                                    label='Nombre'
+                                    sx={{
+                                        display: 'block',
+                                        margin: '0.5rem 0'
+                                    }}
+                                    name='name1'
+                                    inputProps={{style: {color: 'white'}}}
+                                    InputLabelProps={{style: {color:'white'}}}
+                                />
+                                <TextField
+                                    variant='outlined'
+                                    label='Apellido Paterno'
+                                    sx={{
+                                        display: 'block',
+                                        margin: '0.5rem 0'
+                                    }}
+                                    name='name2'
+                                    inputProps={{style: {color: 'white'}}}
+                                    InputLabelProps={{style: {color:'white'}}}
+                                />
+                                <TextField
+                                    variant='outlined'
+                                    label='Apellido Materno'
+                                    sx={{
+                                        display: 'block',
+                                        margin: '0.5rem 0'
+                                    }}
+                                    name='name2'
+                                    inputProps={{style: {color: 'white'}}}
+                                    InputLabelProps={{style: {color:'white'}}}
+                                />
+                                <TextField
+                                    variant='outlined'
+                                    label='Telefono'
+                                    sx={{
+                                        display: 'block',
+                                        margin: '0.5rem 0'
+                                    }}
+                                    name='name2'
+                                    inputProps={{style: {color: 'white'}}}
+                                    InputLabelProps={{style: {color:'white'}}}
+                                />
+                                <TextField
+                                    variant='outlined'
+                                    label='LADA'
+                                    sx={{
+                                        display: 'block',
+                                        margin: '0.5rem 0'
+                                    }}
+                                    name='name2'
+                                    inputProps={{style: {color: 'white'}}}
+                                    InputLabelProps={{style: {color:'white'}}}
+                                />
+                                <TextField
+                                    variant='outlined'
+                                    label='apellidoPat'
+                                    sx={{
+                                        display: 'block',
+                                        margin: '0.5rem 0'
+                                    }}
+                                    name='name2'
+                                    inputProps={{style: {color: 'white'}}}
+                                    InputLabelProps={{style: {color:'white'}}}
+                                />
+                                <Button
+                                    variant='contained'
+                                    color='secondary'
+                                    type='submit'
+                                >
+                                    Guardar
+                                </Button>
+                            </form>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
+        </>
     )
 }
 
